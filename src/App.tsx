b794c4229cbba623591c431c5f6a454dc6b068ff
chandramohan0 <thechandramohan01@gmail.com>
2023-11-21 23:33:15 +0530

    React Router Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 import { EuiProvider } from '@elastic/eui';
 import "@elastic/eui/dist/eui_theme_light.css";
 import React from 'react';
+import {Routes, Route} from "react-router-dom";
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
     <EuiProvider>
-      <div>App</div>
+      <Routes>
+        <Route>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Dashboard />} />
+          <Route path="*" element={<Dashboard />} />
+        </Route>
+      </Routes>
     </EuiProvider>
     
   )
 }
 
-export default App
+export default App;
 
