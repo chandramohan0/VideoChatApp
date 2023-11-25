000e581cd9ea0fe8b83484ef5fd4d8d1baf204a4
chandramohan0 <thechandramohan01@gmail.com>
2023-11-25 16:39:15 +0530

    Designing Login Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { EuiProvider } from '@elastic/eui';
+import { EuiProvider, EuiThemeProvider } from '@elastic/eui';
 import "@elastic/eui/dist/eui_theme_light.css";
 import React from 'react';
 import {Routes, Route} from "react-router-dom";
@@ -6,15 +6,24 @@ import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 
 function App() {
+
+  const overrides = {
+    colors: {
+      LIGHT : {primary: "#0b5cff"},
+      DARK : {primary: "#0b5cff"}
+    },
+  }
   return (
     <EuiProvider>
-      <Routes>
-        <Route>
-          <Route path="/login" element={<Login />} />
-          <Route path="/" element={<Dashboard />} />
-          <Route path="*" element={<Dashboard />} />
-        </Route>
-      </Routes>
+      <EuiThemeProvider modify={overrides}>
+        <Routes>
+          <Route>
+            <Route path="/login" element={<Login />} />
+            <Route path="/" element={<Dashboard />} />
+            <Route path="*" element={<Dashboard />} />
+          </Route>
+        </Routes>
+      </EuiThemeProvider>
     </EuiProvider>
     
   )
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import { EuiButton, EuiFlexGroup, EuiFlexItem, EuiImage, EuiProvider, EuiSpacer, EuiText, EuiTextColor, EuiPanel } from '@elastic/eui';
+import React from 'react';
+import animation from '../assets/animation.gif';
+import logo from '../assets/logo.png';
 
 function Login() {
   return (
-    <div>
-      Login
-    </div>
+    <EuiProvider colorMode='dark'>
+      <EuiFlexGroup alignItems='center' justifyContent='center' style={{width:'100vw', height:'100vh'}}>
+        <EuiFlexItem grow={false}>
+          <EuiPanel paddingSize='xl'>
+            <EuiFlexGroup justifyContent='center' alignItems='center'>
+              <EuiFlexItem>
+                <EuiImage src={animation} alt='logo'></EuiImage>
+              </EuiFlexItem>
+              <EuiFlexItem>
+                <EuiImage src={logo} alt='logo' size='230px' />
+                <EuiSpacer size='xs' />
+                <EuiText textAlign='center' grow={false}>
+                  <h3>
+                    <EuiTextColor>One Platform to</EuiTextColor>
+                    <EuiTextColor color="#0b5cff"> connect</EuiTextColor>
+                  </h3>
+                </EuiText>
+                  <EuiSpacer size='m' />
+                  <EuiButton fill>Login with Google</EuiButton>
+                </EuiFlexItem>
+            </EuiFlexGroup>
+          </EuiPanel>
+        </EuiFlexItem>
+      </EuiFlexGroup>
+    </EuiProvider>
   )
 }
 
