5c5c056e7d70d986438976855043a5eac8759062
chandramohan0 <thechandramohan01@gmail.com>
2023-11-25 17:28:52 +0530

    Login With Google added && Dashboard.jsx file name fixed to Dashboard.tsx

--- src/utils/FirebaseConfig.ts
+++ src/utils/FirebaseConfig.ts
@@ -21,3 +21,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const firebaseAuth = getAuth(app);
 export const firebaseDB = getFirestore(app);
+
+export const userRef = collection(firebaseDB, "users")
